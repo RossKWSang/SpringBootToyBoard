5d3d384f74f0f4fc232f346dab5a252be59fbd92
Wonsang Kim <rosskysoss@gmail.com>
2023-10-24 17:47:31 +0900

    first commit

---
+++ src/main/java/com/fastcampus/springboottoyboard/SpringBootToyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.springboottoyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootToyBoardApplication.class, args);
+	}
+
+}
